57b37ac678b2b64c5a7675caba34e002b811a725
yank.nvim <yanknvim@gmail.com>
2026-04-03 23:06:15 +0900

    add killall

--- hosts/sana/default.nix
+++ hosts/sana/default.nix
@@ -122,6 +122,7 @@
   environment.systemPackages = with pkgs; [
     git
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    killall
     tree
     wget
     grim
